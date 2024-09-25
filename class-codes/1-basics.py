8be771f3820e9b3c41926d33f1cf08ca3e94f11a
ShreyaUPES <Shreya.16431@stu.upes.ac.in>
2024-09-25 11:51:21 +0530

    Updated the file

--- class-codes/1-basics.py
+++ class-codes/1-basics.py
@@ -1,6 +1,18 @@
 #Print statement in python
 print("Hello, World!")
 
+x = 5
+print(id(x))  # Output: Address of x using id function
+x = x + 1
+
+print(id(x))  # Output: New address, since x is now a new object
+y = "Hello"
+
+print(id(y))  # Output: Address of y
+y = y + " World"
+
+print(id(y))  # Output: New address, since y is now a new object
+
 #using input function in python
 x = int(input('Enter value of x: '))
 
@@ -9,6 +21,12 @@ if x > 5:
     print("x is greater than 5.")
 else:
     print("x is lesser than 5.")
+
+#trying out string manipulation
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2  # Output: "Hello World"Repetition
+result = str1 * 3  # Output: "HelloHelloHello"Membership
 
 #seeing how the break statement works, along with while statement
 count = int(input("Enter the value of count: "))
